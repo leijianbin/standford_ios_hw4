67e79c7131477a94442932623b54c8dda9b0e74a
Ray <leijianljb@gmail.com>
2014-05-10 10:01:08 -0700

    Add GameHistoryViewController.h

---
+++ Matchers/GameHistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameHistoryViewController.h
+//  Matchers
+//
+//  Created by Jianbin Lei on 5/10/14.
+//  Copyright (c) 2014 oit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameHistoryViewController : UIViewController
+
+@end
---
+++ Matchers/GameHistoryViewController.m
@@ -0,0 +1,13 @@
+//
+//  GameHistoryViewController.m
+//  Matchers
+//
+//  Created by Jianbin Lei on 5/10/14.
+//  Copyright (c) 2014 oit. All rights reserved.
+//
+
+#import "GameHistoryViewController.h"
+
+@implementation GameHistoryViewController
+
+@end
